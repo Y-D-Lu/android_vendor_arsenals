fbdf624f4c73aaa6e03610755a99c0d9684dd109
luyuedong <e4256318151@gmail.com>
2019-03-16 00:33:02 +0800

    change classes as well as add log

--- SensorOS/frameworks/base/sos/java/cn/arsenals/sos/CastEntrance.java
+++ SensorOS/frameworks/base/sos/java/cn/arsenals/sos/CastEntrance.java
@@ -5,8 +5,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Looper;
 
+import cn.arsenals.sos.util.SosLog;
+
+import java.util.Arrays;
+
 public final class CastEntrance {
+    private static final String TAG = "CastEntrance";
     public static void main(String[] args) {
+        SosLog.d(TAG, "CastEntrance.main : " + Arrays.toString(args));
         Looper.prepare();
         Context ctx = ActivityThread.systemMain().getSystemContext();
         Intent intent = new Intent();
@@ -15,6 +21,7 @@ public final class CastEntrance {
         intent.addFlags(Intent.FLAG_RECEIVER_FOREGROUND);
         intent.putExtra(SosConstants.Broadcast.ARGS, args);
         ctx.sendBroadcast(intent);
+        SosLog.d(TAG, "sentBroadcast, intent : " + intent);
         Looper.loop();
     }
 }
--- SensorOS/frameworks/base/sos/java/cn/arsenals/sos/SosConstants.java
+++ SensorOS/frameworks/base/sos/java/cn/arsenals/sos/SosConstants.java
@@ -4,8 +4,8 @@ public class SosConstants {
     public static final String SOS_PACKAGE_NAME = "cn.arsenals.sos";
 
     public class Broadcast {
-        public static final String RECEIVER_NAME = "cn.arsenals.sos.kastro.CastBroadcastReceiver";
-        public static final String ACTION = "cn.arsenals.sos.kastro.CAST_START";
+        public static final String RECEIVER_NAME = "cn.arsenals.sos.cast.CastBroadcastReceiver";
+        public static final String ACTION = "cn.arsenals.sos.cast.CAST_START";
         public static final String ARGS = "args";
     }
 
